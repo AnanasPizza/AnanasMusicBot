303fae8604be6a61a44110118a1762cce0b32ea6
bgiese <bengiese@protonmail.com>
2024-01-12 02:17:13 +0100

    Connect local dev to repo

---
+++ README.md
@@ -0,0 +1,78 @@
+# AnanasMusicBot
+
+Open Source (CC BY-NC 4.0 DEED) Twitch Spotify Bot, by twitch.tv/ananasxpress_
+
+AnanasMusicBot allows Twitch-Streamers to enable Spotify Songrequests via Channelpoints, by hosting this project. Viewers can also get informations about the current, previous and upcoming songs. Moderators can skip songs or change the volume.
+There is no additional cost, except from hosting a webserver that can run a NodeJS App, that supports https and certificates.
+
+If you need help, hosting this on your server, you can always contact me.
+
+## Prerequisites
+
+- Insomnia REST Caller (https://insomnia.rest/download)
+- Server with nodejs installed
+- Set up a MySQL-Database
+  - Execute the setup.sql to create the initial database structure
+- Create Spotify-App
+  - https://developer.spotify.com/dashboard/create
+  - Write down your Client ID and Client Secret
+- Create a Twitch Account, that is your actual Chat-Bot
+- Create Twitch App
+  - https://dev.twitch.tv/console/apps
+  - Write down your Client ID and Client Secret
+- Setup your webserver, so that it can host a https Webserver, therefore you need to get a certificate
+  - Create a folder "certs"
+  - Add certificate and key as "cert.pem" and "key.pem" to the folder.
+- Create a file '.env', copy everything from .example.env and fill in the values as described in the placeholders.
+
+Once this is done, you need to create the connection between your Bot Account and the Twitch App and allow the Bot to reauthenticate itself
+
+- Log into your Twitch Bot Account
+- Navigate to https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_TWITCH_REDIRECT_URI&scope=chat%3Aread+chat%3Aedit
+- Authenticate the bot, you will then be redirected to the redirect URI.
+- Check the URL in your browser, there will be a query parameter called "code".
+- Copy the value
+- Execute a POST request with Insomnia to:
+  https://id.twitch.tv/oauth2/token?client_id=YOUR_CLIENT_ID8&client_secret=YOUR_CLIENT_SECRET&code=CODE_RETURNED_FROM_PREVIOUS_STEP&grant_type=authorization_code&redirect_uri=YOUR_TWITCH_REDIRECT_URI
+- Log into your Database
+- In table 'bot_tokenstore' add a new row
+- Values:
+  - "twitch_id" -> Twitch ID of the Bot
+  - "accessToken" -> Value of "access_token" from the POST Request
+  - "expires_in" -> Set to 0
+  - "obtainmentTimestamp" -> Set to 0
+  - "refreshToken" -> Value of "refresh_token" from the POST Request
+  - "scope" -> "[chat:edit,chat:read]"
+
+On the next start, the bot should automatically get a new token and persist it.
+
+## Installation
+
+```
+npm install
+```
+
+```
+npm install -g forever
+```
+
+```
+forever start index.js
+```
+
+## Usage
+
+To finally use the bot, start it and then visit YOUR_BASE_URL/start
+
+First connect your Twitch Account and give the bot access to the requested scopes. 
+Then fill in your Twitch ID and Username.
+You will be redirected back to the start and can click on Connect to Spotify.
+Give the Spotify App Access to your account
+The Bot should then join your channel and write a message that it is there. Type !srcommands to check that it works and see all available commands.
+
+If you want to give others the opportunity to use your instance, they have to follow the steps to connect their account and also they need to give you their email address, so that you can add them in the Spotify Developer Console.
+There is a limited space of 25 Users in the Development Mode of a Spotify App.
+
+## License
+
+[CC BY-NC 4.0 DEED](https://creativecommons.org/licenses/by-nc/4.0/)
---
+++ setup.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 10.35.46.77:3306
+-- Erstellungszeit: 12. Jan 2024 um 01:32
+-- Server-Version: 8.0.33
+-- PHP-Version: 8.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `blacklisted_artists`
+--
+
+CREATE TABLE `blacklisted_artists` (
+  `id` int NOT NULL,
+  `channel_id` int NOT NULL,
+  `blocked_artist_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `blacklisted_tracks`
+--
+
+CREATE TABLE `blacklisted_tracks` (
+  `id` int NOT NULL,
+  `channel_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `blocked_track_id` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `blacklisted_users`
+--
+
+CREATE TABLE `blacklisted_users` (
+  `id` int NOT NULL,
+  `channel_id` varchar(255) NOT NULL,
+  `blocked_user_name` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `bot_tokenstore`
+--
+
+CREATE TABLE `bot_tokenstore` (
+  `twitch_id` bigint NOT NULL,
+  `accessToken` varchar(255) NOT NULL,
+  `expiresIn` int NOT NULL,
+  `obtainmentTimestamp` bigint NOT NULL,
+  `refreshToken` varchar(255) NOT NULL,
+  `scope` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `channel_settings`
+--
+
+CREATE TABLE `channel_settings` (
+  `channel_name` varchar(255) NOT NULL,
+  `settings_key` varchar(255) NOT NULL,
+  `settings_val` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `tokenstore`
+--
+
+CREATE TABLE `tokenstore` (
+  `id` int NOT NULL,
+  `twitchid` varchar(255) NOT NULL,
+  `twitchlogin` varchar(255) NOT NULL,
+  `spotifytoken` varchar(255) NOT NULL,
+  `spotifyrefresh` varchar(255) NOT NULL,
+  `spotifyexpiration` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `blacklisted_artists`
+--
+ALTER TABLE `blacklisted_artists`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_artist_channel` (`channel_id`,`blocked_artist_id`);
+
+--
+-- Indizes für die Tabelle `blacklisted_tracks`
+--
+ALTER TABLE `blacklisted_tracks`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `twitchid` (`channel_id`,`blocked_track_id`);
+
+--
+-- Indizes für die Tabelle `blacklisted_users`
+--
+ALTER TABLE `blacklisted_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `channel_id` (`channel_id`,`blocked_user_name`);
+
+--
+-- Indizes für die Tabelle `bot_tokenstore`
+--
+ALTER TABLE `bot_tokenstore`
+  ADD PRIMARY KEY (`twitch_id`);
+
+--
+-- Indizes für die Tabelle `channel_settings`
+--
+ALTER TABLE `channel_settings`
+  ADD UNIQUE KEY `channel_name` (`channel_name`,`settings_key`);
+
+--
+-- Indizes für die Tabelle `tokenstore`
+--
+ALTER TABLE `tokenstore`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `twitchid` (`twitchid`),
+  ADD UNIQUE KEY `twitchlogin` (`twitchlogin`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `blacklisted_artists`
+--
+ALTER TABLE `blacklisted_artists`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `blacklisted_tracks`
+--
+ALTER TABLE `blacklisted_tracks`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `blacklisted_users`
+--
+ALTER TABLE `blacklisted_users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `tokenstore`
+--
+ALTER TABLE `tokenstore`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
